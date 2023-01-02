eff90b362aa0aa2f21e21b4ac20d3210519c2b6f
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-02 22:50:28 +0530

    express todo almost done

--- express-todo-crud/index.js
+++ express-todo-crud/index.js
@@ -1,24 +1,30 @@
 const express=require("express");
-const fs=require("fs");
+const MongoClient = require('mongodb').MongoClient
+var db
 
   const app=express()
 
   app.use(express.json())
 
    app.get("/",(req,res)=>{
-    const data=JSON.parse(fs.readFileSync("./db.json","utf-8"))
-    console.log(data.students)
-    res.send(data.students)
+    // const data=JSON.parse(fs.readFileSync("./db.json","utf-8"))
+    // console.log(data.students)
+    // res.send(data.teachers)
+     res.sendFile(__dirname+"/index.html")
    })
 
-   app.post("/",(req,res)=>{
-    console.log(req.body)
-       res.send("data has been updated!")
+   app.post('/quotes', (req, res) => { 
+    db.collection('quotes').save(req.body, (err, result) => {    if (err) return console.log(err)}
+    })
+
+
+
+
+   MongoClient.connect('mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-atlas?retryWrites=true&w=majority', (err, database) => {  
+    if (err){ return console.log(err) }
+    db = database  
+     app.listen(5000,()=>{
+      console.log("listening on port 5000")
+     })
    })
 
-
-
-
-   app.listen(5000,()=>{
-    console.log("listening on port 5000")
-   })
