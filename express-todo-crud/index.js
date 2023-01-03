da80bf7a3f398620f864dc51631fb595eaaedc44
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-03 21:08:25 +0530

    users part done

--- express-todo-crud/index.js
+++ express-todo-crud/index.js
@@ -1,30 +1,120 @@
-const express=require("express");
-const MongoClient = require('mongodb').MongoClient
-var db
+const express = require('express');
+const mongoose = require('mongoose');
 
-  const app=express()
+const app = express();
+app.use(express.json())
 
-  app.use(express.json())
-
-   app.get("/",(req,res)=>{
-    // const data=JSON.parse(fs.readFileSync("./db.json","utf-8"))
-    // console.log(data.students)
-    // res.send(data.teachers)
-     res.sendFile(__dirname+"/index.html")
-   })
-
-   app.post('/quotes', (req, res) => { 
-    db.collection('quotes').save(req.body, (err, result) => {    if (err) return console.log(err)}
-    })
+const connect=()=>{
+  return mongoose.connect(
+    "mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-atlas?retryWrites=true&w=majority",
+    {useNewUrlParser: true}
+    );
+}
 
 
+//userSchema
+//step 1 creating schema
+const userSchema =new mongoose.Schema(
+  {
+  firstName: {type: String,required: true},
+  lastName: {type: String,required: false},
+  email: {type: String,required: true,unique: true},
+  password: {type: String,required: true}
+
+},
+{
+  timestamps:true,
+}
+);
+
+//Step 2 : Create the model
+const User= mongoose.model('user',userSchema);
+
+//POST schema
+//step 1 creating schema
+const postSchema = new mongoose.Schema(
+  {
+  tite:{type:String, required:true},   
+  body:{type:String, required:true},
+  userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user",
+    required:true,
+  },
+  },
+  {
+  timestamps:true,
+}
+);
+const Post= mongoose.model('post',postSchema);
 
 
-   MongoClient.connect('mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-atlas?retryWrites=true&w=majority', (err, database) => {  
-    if (err){ return console.log(err) }
-    db = database  
-     app.listen(5000,()=>{
-      console.log("listening on port 5000")
-     })
-   })
+//COMMent schema
+//Step 1: Create new schema
+const commentSchema = new mongoose.Schema(
+  {
+    body: {type:String,required:true},
+    postId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"post",
+      required:true
+    },
+    userId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"user",
+      required:true
+    },
+},
+{ 
+  timestamps:true,
+}
+);
 
+//step 2: Creating the model
+const Comment = mongoose.model('comment',commentSchema)
+
+
+//Crud operations 
+//get => getting data from the server
+//post => adding data to the server
+//put/patch => updating data from the server
+//delete => deleting data from the server
+
+//users CRUD
+app.get('/users',async(req, res) => {
+  try{
+    const users = await User.find().lean().exec();
+     res.status(200).send({users: users})
+  }catch(err){
+    return res
+    .status(500)
+    .send({message:"something went wrong"})
+
+  }
+})
+
+app.post('/users',async(req, res) => {
+  try{
+     const user = await User.create(req.body)
+     res.status(201).send({user: user})
+     console.log("data added successfully")
+
+  }catch(err){
+    return res
+    .status(500)
+    .send({message:err.message})
+
+  }
+})
+
+
+
+mongoose.set('strictQuery', false);
+app.listen(5000,async()=>{
+  try{
+    await connect();
+  }catch(err){
+    console.log(err);
+  }
+  console.log("listening on port 5000")
+})
