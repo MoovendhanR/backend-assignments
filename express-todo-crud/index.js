e57fb18588f0309327c5f1763053ec11561352ae
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-05 11:07:24 +0530

    comment section fully updated

--- express-todo-crud/index.js
+++ express-todo-crud/index.js
@@ -34,7 +34,7 @@ const User= mongoose.model('user',userSchema);
 //step 1 creating schema
 const postSchema = new mongoose.Schema(
   {
-  tite:{type:String, required:true},   
+  title:{type:String, required:true},   
   body:{type:String, required:true},
   userId:{
     type:mongoose.Schema.Types.ObjectId,
@@ -110,37 +110,14 @@ app.post('/users',async(req, res) => {
 })
 
 
-//posts
-app.get('/posts',async(req, res) => {
-  try{
-    const posts = await Post.find().lean().exec();
-     res.status(200).send({posts: posts})
-  }catch(err){
-    return res
-    .status(500)
-    .send({message:"something went wrong"})
-
-  }
-});
-app.post('/posts',async(req, res) => {
-    try{
-      const  post= await Post(req.body)
-      res.status(201).send({post: post})
-
-    }catch(err){
-      res.status(500).send({message:err.message})
-    }
-})
-
-
 
 app.get('/users/:id',async(req, res) => {
   try{
-   // console.log(req.params)
-    const user = await User.findById(req.params.id);
+    // console.log(req.params)
+    const user = await User.findById(req.params.id).lean().exec();
     res.status(200).send( user)
      // console.log(users.find({firstName:"nari"}))
-  }catch(err){
+    }catch(err){
     return res
     .status(500)
     .send({message:err.message})
@@ -153,7 +130,7 @@ app.get('/users/:id',async(req, res) => {
 app.patch('/users/:id',async(req, res) => {
   try{
    // console.log(req.params)
-    const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
     //new:true instant update
     res.status(200).send( user)
      // console.log(users.find({firstName:"nari"}))
@@ -166,6 +143,132 @@ app.patch('/users/:id',async(req, res) => {
 })
 
 
+app.delete('/users/:id',async(req, res) => {
+  try{
+    // console.log(req.params)
+    const user = await User.findByIdAndDelete(req.params.id).lean().exec();
+    res.status(200).send(user)
+  }catch(err){
+    return res
+    .status(500)
+    .send({message:err.message})
+  }
+})
+
+
+
+
+
+//POSTS crud
+app.get('/posts',async(req, res) => {
+  try{
+    const posts = await Post.find()
+    //.populate("userId")
+    .populate({
+      path:"userId",
+      select:{firstName:1,_id:0,email:1}
+    })
+    .lean()
+    .exec();
+     res.status(200).send({posts: posts})
+  }catch(err){
+    return res
+    .status(500)
+    .send({message:"something went wrong"})
+
+  }
+});
+app.post('/posts',async(req, res) => {
+    try{
+      const  post= await Post(req.body);
+      res.status(201).send({post: post})
+
+    }catch(err){
+      res.status(500).send({message:err.message})
+    }
+})
+
+app.get('/posts/:id',async(req, res) => {
+  try{
+   // console.log(req.params)
+    const post = await Post.findById(req.params.id).lean().exec();
+    res.status(200).send(post)
+     // console.log(users.find({firstName:"nari"}))
+  }catch(err){
+    return res
+    .status(500)
+    .send({message:err.message})
+
+  }
+})
+
+app.patch("/posts/:id",async(req, res) => {
+   try{
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body,{new:true}).lean().exec();
+     res.status(200).send(post);
+   }catch(err){
+    return res
+    .status(500)
+    .res.send({message:err.message})
+   }
+})
+
+app.delete("/posts/:id",async(req,res)=>{
+    try{
+
+     const post = await Post.findByIdAndDelete(req.params.id).lean().exec();
+     res.status(200).send(post)
+
+    }catch(err){
+      res
+      .status(500)
+      .send({message:err.message});
+    }
+})
+
+//COMMENTS crud
+app.get("/comments",async(req, res)=>{
+  try{
+    const comments = await Comment.find()
+    .lean()
+    .exec();
+    res.status(200).send({comments: comments});
+
+
+  }catch(err){
+       res
+       .status(500)
+       .send({message:err.message});
+  }
+})
+app.post("/comments",async(req, res)=>{
+  try{
+    const comment = await Comment.create(req.body);
+    res.status(200).send({comment: comment});
+
+  }catch(err){
+       res
+       .status(500)
+       .send({message:err.message});
+  }
+})
+app.patch("/comments/:id",async(req, res)=>{
+  try{
+  const comment=await Comment.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
+  res.status(200).send(comment);
+  }catch(err){
+    res.status(500).send({message:err.message});
+  }
+})
+
+app.delete("/comments/:id",async(req, res)=>{
+  try{
+  const comment=await Comment.findByIdAndDelete(req.params.id).lean().exec();
+  res.status(200).send(comment);
+  }catch(err){
+    res.status(500).send({message:err.message});
+  }
+})
 
 
 
