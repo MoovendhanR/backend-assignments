103da6bb9ec778de534e60d0f34cd7c326e742f7
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-13 10:19:41 +0530

    full stack crud -backend initializes

---
+++ fullstack-crud/backend/src/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const mongoose  = require('mongoose');
+const connect = require('./config/db');
+
+const app = express();
+
+app.use(express.json());
+
+
+
+
+mongoose.set("strictQuery",true);
+app.listen(5000,async()=>{
+    await connect();
+    console.log('listening on port 5000')
+})
