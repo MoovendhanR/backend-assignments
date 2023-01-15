c0247ff140b5eb002f3bbfb28b70104244498b2f
MoovendhanR <moovendhanr1011@gmail.com>
2023-01-15 17:17:04 +0530

    notes part authenticate completed"

--- fullstack-crud/backend/src/index.js
+++ fullstack-crud/backend/src/index.js
@@ -2,13 +2,15 @@ const express=require('express');
 const mongoose  = require('mongoose');
 const connect = require('./config/db');
 const userController=require("./controllers/user.controller.js")
-const notesController = require("./controllers/note.controller.js")
+const notesController = require("./controllers/note.controller.js");
+const authenticate = require('./middlewares/authenticate.middleware.js');
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users",userController)
+app.use(authenticate)
 app.use("/notes",notesController)
 
 
---
+++ fullstack-crud/backend/src/middlewares/authenticate.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+
+
+const authenticate = (req, res, next) => {
+     const token = req.headers.authorization;
+     if(token){
+        const decoded = jwt.verify(token,"masai");
+        if(decoded){
+            next();
+        }else{
+            res.status(200).send("Please Login First");
+        }
+     }else{
+        res.status(200).send("Please Login First")
+     }
+}
+
+
+module.exports = authenticate;
